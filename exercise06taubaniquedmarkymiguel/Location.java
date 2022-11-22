191f311b2da760807e5eed2d1531dd39b3c4784d
murrkii 3 <99413747+murrkii@users.noreply.github.com>
2022-11-22 08:56:56 +0800

    Add files via upload
    CHANGE NAME TO EX07 CUNT

---
+++ exercise06taubaniquedmarkymiguel/Location.java
@@ -0,0 +1,32 @@
+package exercise06taubaniquedmarkymiguel;
+import java.util.*;
+
+public class Location {
+    private String name;
+    private ArrayList<Character> personList;
+    private String localSpecialty;
+
+    public Location(String n){
+        this.name = n;
+        this.localSpecialty = null;
+        this.personList = new ArrayList<>();
+    }
+    public Location(String n, String s){
+        this.name = n;
+        this.localSpecialty = s;
+        this.personList = new ArrayList<>();
+    }
+
+    public String getName(){
+        return name;
+    }
+    public ArrayList<Character> getPersonList(){
+        return personList;
+    }
+    public void addPerson(Character c){
+        personList.add(c);
+    }
+    public void removePerson(Character c){
+        personList.remove(c);
+    }
+}
